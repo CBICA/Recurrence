6e9507c7a21c35524d4110844dda6b94b6b53bb5
Ashish Singh <ashish.singh@uphs.upenn.edu>
2021-04-01 12:51:42 -0400

    adding files

---
+++ Modules/CbicaCommon/files.cmake
@@ -0,0 +1,29 @@
+set(CPP_FILES
+	DownloadManager.cpp
+	CaPTkGUIUtils.cpp
+)
+
+set(UI_FILES
+
+)
+
+set(H_FILES
+  include/CaPTkClassifierUtils.h
+  include/CaPTkDefines.h
+  include/CaPTkEnums.h
+  include/CaPTkGUIUtils.h
+  include/CaPTkUtils.h
+  include/DownloadManager.h
+)
+
+set(MOC_H_FILES
+  include/DownloadManager.h
+)
+
+set(RESOURCE_FILES
+ 
+)
+
+set(QRC_FILES
+  
+)

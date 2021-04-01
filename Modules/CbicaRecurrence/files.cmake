554838d29c12ff06474f32eccc00849de14cc1f1
Ashish Singh <ashish.singh@uphs.upenn.edu>
2021-04-01 14:03:33 -0400

    adding files

---
+++ Modules/CbicaRecurrence/files.cmake
@@ -0,0 +1,33 @@
+set(CPP_FILES
+  RecurrenceEstimator.cpp
+  FeatureExtractionClass.cpp
+  FeatureReductionClass.cpp
+  FeatureScalingClass.cpp
+  NiftiDataManager.cpp
+  OutputWritingManager.cpp
+)
+
+set(UI_FILES
+
+)
+
+set(H_FILES
+  include/RecurrenceEstimator.h
+  include/FeatureExtractionClass.h
+  include/FeatureReductionClass.h
+  include/FeatureScalingClass.h
+  include/NiftiDataManager.h
+  include/OutputWritingManager.h
+)
+
+set(MOC_H_FILES
+
+)
+
+set(RESOURCE_FILES
+ 
+)
+
+set(QRC_FILES
+  
+)
